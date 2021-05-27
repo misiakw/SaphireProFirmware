7141170731f1eea92808dee1f5a89c658c767600
misiakw <misiakw@gmail.com>
2021-05-27 09:21:19 +0200

    version 2.0.8 configured

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1012,7 +1012,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 55
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
@@ -1178,8 +1178,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
